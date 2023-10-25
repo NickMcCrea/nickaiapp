18f1c466aa0a5c89bf3413f9329c5b405430efdd
NickMcCrea <nickmccrea@gmail.com>
2023-10-25 13:19:05 +0100

    Basic table component

--- nick-react-app/src/App.tsx
+++ nick-react-app/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState,useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Resizable, ResizableBox } from 'react-resizable'; // New import
 import ChatService from './Services/ChatService';
 import ChatHistory, { Message } from './Components//ChatHistory'; // Adjust the import path as needed
@@ -8,11 +8,21 @@ import AIChatBox from './Components/AIChatBox'; // New import
 import { Grid } from '@mui/material'; // New import
 import "react-resizable/css/styles.css"; // New import for styling
 import './App.css';
+import BasicTable from './Components/BasicTable';
 
 function App() {
   const [messages, setMessages] = useState<Message[]>([]);
   const [estimatedCost, setEstimatedCost] = useState<string>(""); // New state variable
   const [selectedModel, setSelectedModel] = useState('GPT3.5'); // New state variable for selected model
+
+
+  // Dummy data for the table
+  const dummyData = [
+    { Name: 'Alice', Age: 30, Email: 'alice@email.com' },
+    { Name: 'Bob', Age: 40, Email: 'bob@email.com' },
+    { Name: 'Charlie', Age: 50, Email: 'charlie@email.com' }
+  ];
+
 
   const handleModelChange = (model: string) => { // New function to handle model change
     setSelectedModel(model);
@@ -30,11 +40,11 @@ function App() {
   const handleSendMessage = async (content: string) => {
     // Append user's message to ChatHistory
 
-    
+
     const chatService = new ChatService("http://localhost:5001");
 
     setMessages([...messages, { type: 'text', content, timestamp: new Date(), sender: 'You' }]);
-    
+
     try {
       // Get the reply from the server
       const reply = await chatService.sendMessage(content, selectedModel);
@@ -43,12 +53,12 @@ function App() {
       console.log(selectedModel);
 
       const estimatedCost = reply.estimated_cost; // Extract this from the reply
-      
+
       //print this to the console
       console.log(estimatedCost);
       setEstimatedCost(reply.estimated_cost.toString()); // Update estimatedCost state
-     
-      
+
+
       // Append the server's reply to ChatHistory
       setMessages(prevMessages => [...prevMessages, { type: 'text', content: reply.output, timestamp: new Date(), sender: 'Assistant' }]);
     } catch (error) {
@@ -59,24 +69,29 @@ function App() {
 
   return (
     <div className="App">
-     
-      <Header estimatedCost={estimatedCost} selectedModel={selectedModel} onModelChange={handleModelChange}/>
+
+      <Header estimatedCost={estimatedCost} selectedModel={selectedModel} onModelChange={handleModelChange} />
       <Grid container spacing={3}>
         <Grid item>
           {/* Wrap AIChatBox inside ResizableBox component */}
-          <ResizableBox 
-            width={width} 
-            height={300} 
-            onResize={onResize} 
-            minConstraints={[100, 300]} 
+          <ResizableBox
+            width={width}
+            height={300}
+            onResize={onResize}
+            minConstraints={[100, 300]}
             maxConstraints={[800, 300]}
             handle={<span className="react-resizable-handle react-resizable-handle-e"></span>}
           >
             <div style={{ width, height: 300 }}>
-              <AIChatBox messages={messages} handleSendMessage={handleSendMessage}/>
+              <AIChatBox messages={messages} handleSendMessage={handleSendMessage} />
             </div>
           </ResizableBox>
+
+
         </Grid>
+        <Grid item xs={9} display="flex" flexDirection="column" justifyContent="center" alignItems="center">
+    <BasicTable data={dummyData} />
+</Grid>
       </Grid>
     </div>
   );
