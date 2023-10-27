bb5a5046f4a54756ffbef0b67624ad3cdcc42df3
NickMcCrea <nickmccrea@gmail.com>
2023-10-27 15:08:07 +0100

    light refactoring

--- backend/app.py
+++ backend/app.py
@@ -6,7 +6,7 @@ from flask_cors import CORS
 from uuid import uuid4
 from ConversationHistory import ConversationHistory
 import json
-from functions import Functions
+from functions_wrapper import FunctionsWrapper
 
 
 load_dotenv()
@@ -20,7 +20,7 @@ gpt_3 = "gpt-3.5-turbo-0613"
 gpt_4 = "gpt-4-0613"
 current_model = gpt_3
 user_sessions = {}
-functions = Functions()
+functions = FunctionsWrapper()
 
 
 
@@ -30,36 +30,14 @@ COSTS = {
     "gpt-4-0613": {"input": 0.03 / 1000, "output": 0.006 / 1000}
 }
 
-def get_current_weather(location, unit="fahrenheit"):
-    """Get the current weather in a given location"""
-    weather_info = {
-        "location": location,
-        "temperature": "72",
-        "unit": unit,
-        "forecast": ["sunny", "windy"],
-    }
-    return json.dumps(weather_info)
+
 
 
 @app.route('/ask', methods=['POST'])
 def ask():
 
     #get session ID from the session object
-    session_id = session.get('session_id')
-
-    
-    #check if the user_sessions dictionary has a key for the current session ID
-
-    if session_id not in user_sessions:
-        #print that we're creating a new session ID
-        print("Creating new convo history...")
-        user_sessions[session_id] = ConversationHistory()
-
-    #get the conversation history for the current session ID
-    conversation_history = user_sessions.get(session_id)
-
-    #print session ID
-    print("session_id: ", session_id)
+    conversation_history = get_convo_history()
 
     user_input = request.json.get('input', '')
 
@@ -89,48 +67,71 @@ def ask():
             function_call="auto"
         )
 
-        #function call test
+       
         response_message = response["choices"][0]["message"]
         function_response = None
         was_function_call = response_message.get("function_call")
+
         if was_function_call:
-            #print 
-            print("function call: ", response_message.get("function_call"))
-            available_functions = {
-            "get_current_weather": get_current_weather,
-             }  # only one function in this example, but you can have multiple
-            function_name = response_message["function_call"]["name"]
-            function_to_call = available_functions[function_name]
-            function_args = json.loads(response_message["function_call"]["arguments"])
-            function_response = function_to_call(
-                location=function_args.get("location"),
-                unit=function_args.get("unit"),
-            )
+            function_response = get_function_response(response_message)
 
 
+        chat_output = response['choices'][0]['message']['content']
+        if chat_output is None:
+            chat_output = ""
 
-        assistant_output = response['choices'][0]['message']['content']
-        conversation_history.add_assistant_message(assistant_output)  # Add assistant output to history
+        conversation_history.add_assistant_message(chat_output)  # Add assistant output to history
 
 
-        tokens_used = response['usage']['total_tokens']
-        input_tokens = response['usage']['prompt_tokens']
-        output_tokens = tokens_used - input_tokens
+        input_tokens, output_tokens = calculate_tokens(response)
 
         # Calculate estimated cost
-        input_cost = COSTS[current_model]['input'] * input_tokens
-        output_cost = COSTS[current_model]['output'] * output_tokens
-        estimated_cost = input_cost + output_cost
-        
+        estimated_cost = calculate_costs(input_tokens, output_tokens)     
         cost_so_far = conversation_history.get_total_estimated_cost()
         conversation_history.set_total_estimated_cost(cost_so_far + estimated_cost)
 
         print("Total estimated cost: {:.10f}".format(conversation_history.get_total_estimated_cost()))
 
-        return jsonify({'function_response': function_response, 'function_call':  was_function_call, 'output': assistant_output, 'estimated_cost': "{:.10f}".format(conversation_history.get_total_estimated_cost())}), 200
+        return jsonify({'function_response': function_response, 'function_call':  was_function_call, 'output': chat_output, 'estimated_cost': "{:.10f}".format(conversation_history.get_total_estimated_cost())}), 200
 
     except Exception as e:
         return jsonify({'error': str(e)}), 500
+
+def get_convo_history():
+    session_id = session.get('session_id')
+
+    #check if the user_sessions dictionary has a key for the current session ID
+    if session_id not in user_sessions:
+        #print that we're creating a new session ID
+        print("Creating new convo history...")
+        user_sessions[session_id] = ConversationHistory()
+
+    #get the conversation history for the current session ID
+    conversation_history = user_sessions.get(session_id)
+
+    #print session ID
+    print("session_id: ", session_id)
+    return conversation_history
+
+def get_function_response(response_message):
+    function_name = response_message["function_call"]["name"]
+    #print("function_name: ", function_name)
+    function_args = json.loads(response_message["function_call"]["arguments"])
+    #print("function_args: ", function_args)
+    function_response = functions.execute_function(function_name, function_args)
+    return function_response
+
+def calculate_costs(input_tokens, output_tokens):
+    input_cost = COSTS[current_model]['input'] * input_tokens
+    output_cost = COSTS[current_model]['output'] * output_tokens
+    estimated_cost = input_cost + output_cost
+    return estimated_cost
+
+def calculate_tokens(response):
+    tokens_used = response['usage']['total_tokens']
+    input_tokens = response['usage']['prompt_tokens']
+    output_tokens = tokens_used - input_tokens
+    return input_tokens,output_tokens
 
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
---
+++ backend/functions_wrapper.py
@@ -0,0 +1,61 @@
+import json
+
+#constructor for a functions class
+class FunctionsWrapper:
+
+    #constructor
+    def __init__(self):
+        self.functions = [
+        {
+            "name": "get_current_weather",
+            "description": "Get the current weather in a given location",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "location": {
+                        "type": "string",
+                        "description": "The city and state, e.g. San Francisco, CA",
+                    },
+                    "unit": {"type": "string", "enum": ["celsius", "fahrenheit"]},
+                },
+                "required": ["location"],
+            },
+
+
+        },
+       
+        
+        ]
+
+        self.function_mapping = {
+            "get_current_weather": self.get_current_weather
+            # Add more function mappings here...
+        }
+    
+        
+    #getter
+    def get_functions(self):
+        return self.functions
+    
+    def get_current_weather(self, location, unit="fahrenheit"):
+        """Get the current weather in a given location"""
+        weather_info = {
+            "location": location,
+            "temperature": "72",
+            "unit": unit,
+            "forecast": ["sunny", "windy"],
+        }
+        return json.dumps(weather_info)
+    
+    
+
+    
+    def execute_function(self, name, args):
+ 
+        if name in self.function_mapping:
+            func = self.function_mapping[name]
+            return func(**args)
+        else:
+            raise ValueError(f"Function '{name}' not found.")
+        
+    
