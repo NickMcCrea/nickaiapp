2846ccdecfefe215f357a733c2afdf0f5234db51
NickMcCrea <nickmccrea@gmail.com>
2023-11-24 15:15:54 +0000

    Refactor pipeline visualizer to improve readability and modularity

--- nick-react-app/src/Components/Charts/PipelineVisualiser.tsx
+++ nick-react-app/src/Components/Charts/PipelineVisualiser.tsx
@@ -51,109 +51,74 @@ const PipelineVisualiser: React.FC<PipelineVisualiserProps> = ({ pipelineDefinit
     const convertToFlowElements = (pipeline: PipelineStep[]): void => {
       const horizontalSpacing = 250; // Adjust spacing between nodes
       let xPos = 0;
+      let yPos = 300;
+      let xPosData = 100;
+
 
       const newNodes: Node[] = [];
       const newEdges: Edge[] = [];
       // Determine if the node is a data source node
-    
+      let firstNonDataSourceNode = true;
+
       pipeline.forEach((step, index) => {
 
-        const isDataSourceNode = step.action === 'load_from_service';
+        //if the pipeline step action is load_from_service, create at teh top of the graph
+        if (step.action === 'load_from_service') {
 
-        // Create action node
-        newNodes.push({
-          id: step.id,
-          type: 'default',
-          data: {
-            label: step.action === 'load_from_service' ? step.params.data_source_name : (
-              <>
-                <strong>Action:</strong> {step.action}
-                <br />
-                <strong>Params:</strong> {JSON.stringify(step.params, null, 2)}
-              </>
-            )
-          },
-          sourcePosition: Position.Right,
-          targetPosition: Position.Left,
-          position: { x: xPos, y: 100 },
-          style: {
-            backgroundColor: getColorForAction(step.action),
-            color: '#fff', // Set text color to white
-            border: '1px solid #333',
-            boxShadow: '0 2px 10px rgba(0,0,0,0.15)',
-            ...(isDataSourceNode ? { width: 180, height: 50, fontSize: 16 } : {}), // Example size, adjust as needed
-          }
-        });
-
-        // Increment x position after the load action node
-        
-          xPos += horizontalSpacing;
-        
-
-        // Special case for the first load action to connect directly to the next action
-        if (index === 0 && step.action === 'load_from_service' && pipeline.length > 1) {
-          const nextStep = pipeline[1]; // Assuming there's at least one more step
-          newEdges.push({
-            id: `e${step.id}-${nextStep.id}`,
-            source: step.id,
-            target: nextStep.id,
-            animated: true,
-            style: { stroke: '#333', strokeWidth: 2 },
-
-          });
-        }
-
-        // If this is not a load action, increment x position for the data set node
-
-
-        // Link this action node to the previous data set node, if this is not the first node
-        if (index > 0) {
-          const previousDataSetNodeId = `${pipeline[index - 1].id}-data-set`;
-          newEdges.push({
-            id: `e${previousDataSetNodeId}-${step.id}`,
-            source: previousDataSetNodeId,
-            target: step.id,
-            animated: true,
-            style: { stroke: '#333', strokeWidth: 2 }
-          });
-        }
-
-        // Create data set node after non-load actions
-        if (step.action !== 'load_from_service') {
-          const dataSetNodeId = `${step.id}-data-set`;
-
+          //add a new node
           newNodes.push({
-            id: dataSetNodeId,
+            id: step.id + '-data-source',
+            type: 'input',
+            position: { x: xPosData, y: 0 },
+            data: { label: step.params.data_source_name },
+            style: { background: getColorForAction(step.action), color: 'white' },
+          });
+
+          xPosData += horizontalSpacing;
+
+        }
+
+        if (step.action !== 'load_from_service') {
+
+          //add a node to the left, using the input data source name of this step
+
+          //is this the first step that is not a data source?
+          if (firstNonDataSourceNode) {
+
+            createDataSourceNode(newNodes, step.id + '-left',step.params.name, step, xPos, yPos, getColorForAction);
+            firstNonDataSourceNode = false;
+          }
+
+          xPos += horizontalSpacing;
+
+          //add a new node
+          newNodes.push({
+            id: step.id,
             type: 'default',
-            data: { label: step.params.name },
-            position: { x: xPos, y: 100 },
+            position: { x: xPos, y: yPos },
+            data: { label: step.action },
             sourcePosition: Position.Right,
             targetPosition: Position.Left,
-            style: {
-              backgroundColor: '#0D9CD9',
-              color: '#fff', // Set text color to white
-              border: '1px solid #333',
-              boxShadow: '0 2px 10px rgba(0,0,0,0.15)',
-              width: 180, // Example size, adjust as needed
-              height: 50, // Example size, adjust as needed
-              //text size
-              fontSize: 14,
-
-            }
+            style: { background: getColorForAction(step.action), color: 'white' },
           });
 
-          // Create edge between action node and data set node
-          newEdges.push({
-            id: `e${step.id}-${dataSetNodeId}`,
-            source: step.id,
-            target: dataSetNodeId,
-            animated: true,
-            style: { stroke: '#333', strokeWidth: 2 }
-          });
+          //if we're a join action, create a data source ABOVE the join node
+          if (step.action === 'join') {
+            
+            createDataSourceNode(newNodes, step.id + '-join',step.params.other_name, step, xPos, yPos-100, getColorForAction);
+  
+          }
 
-          // Increment x position for the next node
+
+
           xPos += horizontalSpacing;
+
+          //add a node to the left, using the input data source name of this step
+          createDataSourceNode(newNodes, step.id + '-right',step.params.name, step, xPos, yPos, getColorForAction);
         }
+
+
+
       });
 
       setNodes(newNodes);
@@ -178,3 +143,15 @@ const PipelineVisualiser: React.FC<PipelineVisualiserProps> = ({ pipelineDefinit
 };
 
 export default PipelineVisualiser;
+
+function createDataSourceNode(newNodes: Node<any>[], id: string, label:string, step: PipelineStep, xPos: number, yPos: number, getColorForAction: (action: string) => string) {
+  newNodes.push({
+    id: id,
+    type: 'default',
+    position: { x: xPos, y: yPos },
+    data: { label: label },
+    sourcePosition: Position.Right,
+    targetPosition: Position.Left,
+    style: { background: getColorForAction('load_from_service'), color: 'white' },
+  });
+}
