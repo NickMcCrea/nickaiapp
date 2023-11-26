f24dfb522ed1628c620da3419cd48be839eed51c
NickMcCrea <nickmccrea@gmail.com>
2023-11-26 15:54:57 +0000

    Update test_input in test_data_pipeline_integration.py

--- backend/test_data_pipeline_integration.py
+++ backend/test_data_pipeline_integration.py
@@ -23,7 +23,7 @@ action_manager = ActionsManager("gpt-4-1106-preview", meta_data_service)
 user_session_state = UserSessionState()
 
 
-test_input = "load balances, then filter it on company code 0302"
+test_input = "load balances, then filter it on company code 0302 - call the output 0302_balances"
 
 data,metadata,commentary = action_manager.function_generate_pipeline_definition(None,None,user_session_state, test_input)
 data2, metadata2, commentary2 = action_manager.execute_pipeline_definition(None,None,user_session_state,"create that","MyNewDataSet", "Data set description")
